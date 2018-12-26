34a29e77642da7865b8bf39d1196c99a20bbb87e
Taesu Hyeon <qvil1127@gmail.com>
2018-12-27 04:17:29 +0900

    Module not found: Can't resolve 'react-qui'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { Button } from "react-qui";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    console.log(Button);
+    return (
+      <div>
+        <Button>Button</Button>
+      </div>
+    );
   }
 }
 
